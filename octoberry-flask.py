8698d2c188b2231cb279b304879ca17088ae83c4
Andrey Stepanov <isteep@gmail.com>
2014-03-18 14:11:22 +0400

    fix coding

--- octoberry-flask.py
+++ octoberry-flask.py
@@ -28,10 +28,10 @@ def contact_submit():
     data_to_send = (request.form['name'], request.form['email'], request.form['message'])
     if not any(data_to_send):
         return result
-    message = 'Name: %s\nEmail: %s\nMessage:\n %s \n' % data_to_send
+    message = u'Name: %s\nEmail: %s\nMessage:\n %s \n' % data_to_send
     current_dir = os.path.dirname(os.path.realpath(__file__))
     with open(current_dir + '/mails/' + str(uuid.uuid4()), 'a') as the_file:
-        the_file.write(message)
+        the_file.write(message.encode('utf8'))
 
     msg = Message("Message from site",
                   sender="team@octoberry.ru",
